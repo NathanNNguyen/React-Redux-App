94222d3666a8c708c57047c060bd916924131077
Nhan <natenguyen51294@gmail.com>
2020-01-23 12:09:22 -0800

    added dependencies

---
+++ my-app/src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+  isLoading: false,
+  activity: null,
+}
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+
+
+    default:
+      return state
+  }
+}
